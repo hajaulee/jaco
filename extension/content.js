beb5bb0361bd572f7a856beface43b63dfc68917
Le Cong Hau <anhlahau.hl@gmail.com>
2024-10-03 00:50:37 +0900

    Keep translator setting

--- extension/content.js
+++ extension/content.js
@@ -1,35 +1,53 @@
 // Translate all the text nodes on the page
 const conveter = new Converter();
 const textarea = document.createElement('textarea');
+let translatorExecuted = false;
 
 function decodeHtmlEntities(str) {
   textarea.innerHTML = str;
   return textarea.value;
 }
 
+async function urlContentToDataUri(url){
+  return  fetch(url)
+          .then( response => response.blob() )
+          .then( blob => new Promise( callback =>{
+              let reader = new FileReader() ;
+              reader.onload = function(){ callback(this.result) } ;
+              reader.readAsDataURL(blob) ;
+          }) ) ;
+}
+
+function readAutoTranslation(){
+  return localStorage.getItem('JacoAutoTranslation');
+}
+
+function saveAutoTranslation(value) {
+  return localStorage.setItem('JacoAutoTranslation', value);
+}
+
 async function translatePageContent(text) {
   return Promise.resolve(conveter.convert(text))
 }
 
-fetch(chrome.runtime.getURL('code_map.json'))
-  .then(res => res.text())
-  .then(res => {
-      conveter.updateCodeMap(JSON.parse(res))
-  })
-
 // Walk through the document and replace text
 async function walkAndTranslate(node) {
+  translatorExecuted = true;
   const walker = document.createTreeWalker(node, NodeFilter.SHOW_TEXT, null, false);
   let textNode = walker.nextNode();
 
   while (textNode) {
     const originText = textNode.textContent.trim();
     const parentNode = textNode.parentNode;
-    if (originText && !['SCRIPT', 'STYLE'].includes(parentNode.nodeName)){      
+    if (
+      originText && 
+      !['SCRIPT', 'STYLE'].includes(parentNode.nodeName) &&
+      !parentNode.getAttribute('data-translator-origin')
+    ){      
       try{
         let translatedText = await translatePageContent(originText);
         textNode.textContent = decodeHtmlEntities(translatedText);
-        parentNode.setAttribute('data-translator-origin', originText.trim());
+        parentNode.setAttribute('data-translator-origin', originText);
         parentNode.classList.add("jaco-text");
       } catch (e){
         // console.log(e, textNode.nodeType, textNode);
@@ -40,6 +58,55 @@ async function walkAndTranslate(node) {
 }
 
 
+/* 
+***************************
+*   INIT
+*
+***************************
+*/
+fetch(chrome.runtime.getURL('code_map.json'))
+  .then(res => res.text())
+  .then(res => {
+      conveter.updateCodeMap(JSON.parse(res))
+  })
+
+window.addEventListener("load", () => {
+  if (readAutoTranslation() == 'true'){
+    walkAndTranslate(document.body).then();
+  }
+});
+
+
+// Download font
+urlContentToDataUri("https://hajaulee.github.io/Houf-Jaco-Maru/new_fonts/ttf/HoufJacoMaru-Regular.ttf")
+  .then((urlData) => {
+    var newStyle = document.createElement('style');
+    newStyle.appendChild(document.createTextNode(`
+      @font-face {
+          font-family: "JacoMaru";
+          src: url("${urlData}");
+      }  
+    `));
+    document.head.appendChild(newStyle);
+  })
+  .catch((err) => {
+    console.error(`Error: ${err}`);
+    
+  })
+
+
+setInterval(() => {
+  if (translatorExecuted){
+    walkAndTranslate(document.body).then();
+  }
+}, 3000);
+/*
+***************************
+*     RUNTIME ACTIONS
+*
+***************************
+*/
+
 // Listen for a message to start translation
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "startTranslation") {
@@ -47,5 +114,14 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       sendResponse({ status: "Chuyển chữ thành công!" });
     });
     return true; // Keep the message channel open for sendResponse
+  }
+
+  if (request.action === "readAutoTranslation"){
+    sendResponse({result: readAutoTranslation()})
+  }
+
+  if (request.action === "saveAutoTranslation"){
+    saveAutoTranslation(request.data);
+    sendResponse({result: readAutoTranslation()});
   }
 });
--- extension/popup.js
+++ extension/popup.js
@@ -1,12 +1,35 @@
 // popup.js
+const activeTabFilter = { active: true, currentWindow: true };
+const el = (elId) => document.getElementById(elId);
 
-document.getElementById('translateBtn').addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { action: "startTranslation" }, (response) => {
-        if (response && response.status) {
-          document.getElementById('status').innerText = response.status;
-        }
-      });
+el('translateBtn').addEventListener('click', () => {
+  chrome.tabs.query(activeTabFilter, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "startTranslation" }, (response) => {
+      if (response && response.status) {
+        el('status').innerText = response.status;
+      }
     });
   });
-  
+});
+
+el('autoTranslation').addEventListener('change', () => {
+  const checked = el('autoTranslation').checked;
+  chrome.tabs.query(activeTabFilter, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      action: 'saveAutoTranslation', data: checked
+    }, (response) => {
+      if (response) {
+        // do nothing;
+      }
+    })
+  })
+})
+
+// Init 
+chrome.tabs.query(activeTabFilter, (tabs) => {
+  chrome.tabs.sendMessage(tabs[0].id, { action: "readAutoTranslation" }, (response) => {
+    if (response) {
+      el('autoTranslation').checked = response.result == 'true';
+    }
+  });
+})
